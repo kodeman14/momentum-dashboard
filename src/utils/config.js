0caace60ce232ce9b902b014d7b5289c62b3ba1b
Ankit Curchorcar <3253095+kodeman14@users.noreply.github.com>
2023-08-04 02:34:02 -0400

    moved env vars to config for easy access (#13)

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,4 @@
-export const serverApi =
-  process.env.REACT_APP_JSON_SERVER + process.env.REACT_APP_GITHUB_REPO;
+const REACT_APP_JSON_SERVER = "https://my-json-server.typicode.com/";
+const REACT_APP_GITHUB_REPO = "kodeman14/momentum-dashboard";
+
+export const serverApi = REACT_APP_JSON_SERVER + REACT_APP_GITHUB_REPO;
