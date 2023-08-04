de0d8fb3364fddc6df4ee905ac47a98307e8404d
kodeman14 <ankit.curchorcar@gmail.com>
2023-08-03 21:09:42 -0400

    fetching data from json server and setting values

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { serverApi } from "./utils/config";
 import Header from "./components/Header";
 
 function App() {
+  const [userData, setUserData] = useState();
+  const [serverErr, setServerErr] = useState();
+  const [isRefresh, setIsRefresh] = useState(false);
+
+  const handleFetchData = () => {
+    fetch(`${serverApi}/users`)
+      .then((res) => res.json())
+      .then((res) => setUserData(res))
+      .catch((err) => setServerErr(err));
+  };
+
+  useEffect(() => {
+    handleFetchData();
+  }, []);
   return (
     <div className="App">
       <Header />
