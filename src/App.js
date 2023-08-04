91afb6facb04cbcff905b5329014199d288d0868
kodeman14 <ankit.curchorcar@gmail.com>
2023-08-03 21:11:14 -0400

    setup for showing user list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+
+import UserList from "./components/UserList";
 import { serverApi } from "./utils/config";
 import Header from "./components/Header";
 
 function App() {
   const [userData, setUserData] = useState();
   const [serverErr, setServerErr] = useState();
-  const [isRefresh, setIsRefresh] = useState(false);
+  // const [isRefresh, setIsRefresh] = useState(false);
 
   const handleFetchData = () => {
     fetch(`${serverApi}/users`)
@@ -21,6 +23,11 @@ function App() {
   return (
     <div className="App">
       <Header />
+      {serverErr ? (
+        <span>Display Error, Fetch Again</span>
+      ) : (
+        <UserList userData={userData} />
+      )}
     </div>
   );
 }
