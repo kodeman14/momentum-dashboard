660f859746625b1be5cec64648e8a3b517fd58ca
Ankit Curchorcar <3253095+kodeman14@users.noreply.github.com>
2023-08-03 21:12:04 -0400

    Feature/header display fetch data (#8)
    * created header for the site
    
    * fetching data from json server and setting values
    
    * using server const from utils
    
    * setup for showing user list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+import UserList from "./components/UserList";
+import { serverApi } from "./utils/config";
+import Header from "./components/Header";
 
 function App() {
+  const [userData, setUserData] = useState();
+  const [serverErr, setServerErr] = useState();
+  // const [isRefresh, setIsRefresh] = useState(false);
+
+  const handleFetchData = () => {
+    fetch(`${serverApi}/users`)
+      .then((res) => res.json())
+      .then((res) => setUserData(res))
+      .catch((err) => setServerErr(err));
+  };
+
+  useEffect(() => {
+    handleFetchData();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {serverErr ? (
+        <span>Display Error, Fetch Again</span>
+      ) : (
+        <UserList userData={userData} />
+      )}
     </div>
   );
 }
---
+++ src/utils/config.js
@@ -0,0 +1,2 @@
+export const serverApi =
+  process.env.REACT_APP_JSON_SERVER + process.env.REACT_APP_GITHUB_REPO;
