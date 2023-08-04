9a7b0310b07336550921fc3ca8b9100a5d89ece5
kodeman14 <ankit.curchorcar@gmail.com>
2023-08-04 00:35:02 -0400

    changed header logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,15 @@
 import React from "react";
-import { FcDoughnutChart, FcMenu } from "react-icons/fc";
+import { FcBiotech, FcMenu } from "react-icons/fc";
 
 function Header() {
   return (
     <div className="flex flex-row text-center justify-between bg-yellow-500 p-5">
       <span className="flex">
         <button>
-          <FcDoughnutChart className="align-middle" />
+          <FcBiotech className="align-middle" />
         </button>
       </span>
       <h1 className="font-bold text-3xl mx-5">Patient Dashboard</h1>
-      {/* TODO : add logo and menu icon */}
       <span className="flex">
         <button>
           <FcMenu className="align-middle" />
